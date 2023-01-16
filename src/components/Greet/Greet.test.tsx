1a0f248736895e62bf5a5398e3b81a68f27588b8
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2023-01-16 11:47:12 +0100

    it or test

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -11,17 +11,19 @@ describe('Greet', () => { // grouping multiple tests, describe() is provided by 
         const expectedMessage = screen.getByText(/hi/i) // screen is an obj provided by RTL that have some method to query the dom
         expect(expectedMessage).toBeInTheDocument() // experct() is a Jest global method
     })
+    // test.only()
+    // test.skip()
 
     // Nested describe to create a hierarchy
     describe('Nested', () => {
-        test('renders with a name', () => {
+        it('renders with a name', () => {
             render(<Greet name='Andrew' />)
             const expectedMessageWithName = screen.getByText(/hi andrew/i)
             expect(expectedMessageWithName).toBeInTheDocument()
         })
         
-        // test.only()
-        // test.skip()
+    // fit()
+    // xit()
     })
     
 })
